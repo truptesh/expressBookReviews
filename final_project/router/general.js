2bff04f9927b2d16b8712268d2667a76583d337c
truptesh <trupteshvasava26@gmail.com>
2025-02-19 02:02:28 -0500

    task 3 done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -33,8 +33,18 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author.toLowerCase()
+  let bookByAuthor = {}
+  for (let isbn in books){
+    if (books[isbn].author.toLowerCase() === author.trim()){
+        bookByAuthor[isbn] = structuredClone(books[isbn])
+    }
+  }
+  if (Object.keys(bookByAuthor).length > 0){
+    return res.status(200).json({bookByAuthor});
+  } else {
+    return res.status(404).json({message: "Book not found"})
+  }
 });
 
 // Get all books based on title
