a42bfef8e42c48d35ec0266ce32b087a500c97be
truptesh <trupteshvasava26@gmail.com>
2025-02-20 19:34:53 -0500

    task 7 done

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,22 +1,42 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
+const session = require('express-session')
 const regd_users = express.Router();
 
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+let existUser = users.filter(user => user.username === username.trim())
+return existUser.length > 0;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+const existUser = users.filter(user => user.username === username.trim() && user.password === password.trim())
+return existUser.length > 0;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body
+
+  if (username.trim() === "" || password.trim() === ""){
+    return res.status(400).json({message: "username and password should be not empty"})
+  }
+
+  if (isValid(username)){
+    if (authenticatedUser(username, password)){
+        const accessToken = jwt.sign({data: password}, "JWTsecret", {expiresIn: 60 * 60})
+        // req.session.authorization = accessToken, username
+        return res.status(200).json({message: "user successfullt logged in."})
+    } else {
+        return res.status(403).json({message: "password must be correct"})
+    }
+  } else {
+    return res.status(409).json({message: "user not found, please register first"})
+  }
 });
 
 // Add a book review
